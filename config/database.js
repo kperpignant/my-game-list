b0f784e916aae2c39882031c1f8232e1b0e8ea73
karlee <kperpignant@gmail.com>
2025-11-03 10:44:36 -0500

    hiding key

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb+srv://kperpignant_db_user:ktc9kmGOe4kTwDon@cluster0.ai7bhzl.mongodb.net/MyGameList?retryWrites=true&w=majority', 
+    'url' : MONGO_URI, 
     'dbName': 'MyGameList' //my-game-list
 };
